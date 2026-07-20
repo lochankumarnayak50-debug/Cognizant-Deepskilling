cb68b30b47c0e19a55ef957213d3dcd0a88ca6d1
Lochan Kumar Nayak <lochankumarnayak50@gmail.com>
2026-07-20 12:45:48 +0530

    Add files via upload

---
+++ DotNet-FSE-main/DotNet-FSE-main-Deepskilling/week1/Engineering concepts/Design Patterns and Principles/Exercise 1 Implementing the Singleton Pattern/SingletonPatternExample.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+interface IDocument
+{
+    void Open();
+}
+
+
+class WordDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Word Document");
+    }
+}
+
+
+class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening PDF Document");
+    }
+}
+
+
+class ExcelDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Excel Document");
+    }
+}
+
+
+
+abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+
+class WordDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+
+class PdfDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+
+class ExcelDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+
+
+class Program
+{
+    class FactoryMethodPatternExample
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        IDocument word = wordFactory.CreateDocument();
+        word.Open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        IDocument pdf = pdfFactory.CreateDocument();
+        pdf.Open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        IDocument excel = excelFactory.CreateDocument();
+        excel.Open();
+    }
+    
+}
+}
