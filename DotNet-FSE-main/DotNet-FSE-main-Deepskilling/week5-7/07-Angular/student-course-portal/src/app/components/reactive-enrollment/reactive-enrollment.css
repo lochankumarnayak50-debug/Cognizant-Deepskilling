011a37f7836b39b4a9faaa353b45f47bacfee48b
Lochan Kumar Nayak <lochankumarnayak50@gmail.com>
2026-07-27 10:30:59 +0530

    Add files via upload

---
+++ DotNet-FSE-main/DotNet-FSE-main-Deepskilling/week5-7/07-Angular/student-course-portal/src/app/components/reactive-enrollment/reactive-enrollment.css
@@ -0,0 +1,15 @@
+input,
+select {
+    width: 250px;
+    padding: 6px;
+    margin-bottom: 10px;
+}
+
+button {
+    margin-right: 10px;
+    padding: 8px 15px;
+}
+
+div {
+    font-size: 14px;
+}
