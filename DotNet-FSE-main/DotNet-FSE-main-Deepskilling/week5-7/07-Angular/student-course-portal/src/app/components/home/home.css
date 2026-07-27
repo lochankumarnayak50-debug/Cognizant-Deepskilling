b393993a2b4453ea2090d620694a4f491c9fb1fc
Lochan Kumar Nayak <lochankumarnayak50@gmail.com>
2026-07-27 10:22:27 +0530

    Add files via upload

---
+++ DotNet-FSE-main/DotNet-FSE-main-Deepskilling/week5-7/07-Angular/student-course-portal/src/app/components/home/home.css
@@ -0,0 +1,30 @@
+img {
+  margin: 10px 0;
+}
+
+button {
+  padding: 8px 15px;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+
+input {
+  padding: 6px;
+  width: 250px;
+  margin-bottom: 10px;
+}
+
+hr {
+  margin: 20px 0;
+}
+form {
+  margin-top: 20px;
+}
+
+label {
+  font-weight: bold;
+}
+
+button {
+  margin-top: 10px;
+}
